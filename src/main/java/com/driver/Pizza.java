3536b12c12e31ab6509632c942914863b29354e4
ankitasaha101 <ankitasaha101@gmail.com>
2023-02-06 15:11:22 +0530

    Answer

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,98 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    int toppingVeg = 70;
+    int toppingNonVeg = 120;
+    int paperBagP = 20;
+    int cheeseP = 80;
+    int vegPizzaBaseP = 300;
+    int nonVegBaseP = 400;
 
-    public Pizza(Boolean isVeg){
+    String cheeseBill = "Extra Cheese Added:" + " ";
+    String toppingBill = "Extra Toppings Added:" + " ";
+    String paperBagBill = "Paperbag Added:" + " ";
+    String totalBill = "Total Price:" + " ";
+    String pizzaBaseBill = "Base Price Of The Pizza:" + " ";
+    boolean cFlag = false;
+    boolean tFlag = false;
+    boolean pFlag = false;
+
+
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
+        if (isVeg) {
+            price = vegPizzaBaseP;
+
+        } else {
+            price = nonVegBaseP;
+
+        }
         // your code goes here
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
+    public void addExtraCheese() {
         // your code goes here
+        if (!cFlag) {
+            price += cheeseP;
+            cFlag = true;
+        }
+
     }
 
-    public void addExtraToppings(){
+    public void addExtraToppings() {
         // your code goes here
+        if (!tFlag) {
+            if (isVeg) {
+                price += toppingVeg;
+
+            } else {
+                price += toppingNonVeg;
+            }
+            tFlag = true;
+        }
+
     }
 
-    public void addTakeaway(){
+    public void addTakeaway() {
         // your code goes here
+        if (!pFlag) {
+            price += paperBagP;
+            pFlag = true;
+        }
+
     }
 
-    public String getBill(){
+    public String getBill() {
         // your code goes here
+        if (isVeg) {
+            bill = pizzaBaseBill + vegPizzaBaseP + "\n";
+        }
+
+        if (!isVeg) {
+            bill = pizzaBaseBill + nonVegBaseP + "\n";
+        }
+
+
+        if (cFlag) {
+            bill += cheeseBill + cheeseP + "\n";
+        }
+        if (tFlag) {
+            if (isVeg) {
+                bill += toppingBill + toppingVeg + "\n";
+            } else {
+                bill += toppingBill + toppingNonVeg + "\n";
+            }
+        }
+        if (pFlag) {
+            bill += paperBagBill + paperBagP + "\n";
+        }
+        bill += totalBill + price + "\n";
+        this.bill = bill;
+
         return this.bill;
     }
 }
